13725865ab5b01c5ac167e86ea4d0033f650ba21
Kyryl <kyryl.pochoma@microsoft.wsei.edu.pl>
2025-05-30 19:11:45 +0200

    added class Order

---
+++ ShawarmaShop/Models/Order.cs
@@ -0,0 +1,8 @@
+﻿public class Order
+{
+    public int Id { get; set; }
+    public DateTime DateTime { get; set; }
+    public string? Comment { get; set; }
+    public int ClientID { get; set; }
+
+}

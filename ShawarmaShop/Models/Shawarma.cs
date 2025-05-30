5d9aee1248eee4f06127590addaf8b9b527154aa
Kyryl <kyryl.pochoma@microsoft.wsei.edu.pl>
2025-05-30 19:07:37 +0200

    new folder Models added new class Shawarma added

---
+++ ShawarmaShop/Models/Shawarma.cs
@@ -0,0 +1,8 @@
+﻿public class Shawarma
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Ingredients { get; set; } = null!;
+    public decimal Price { get; set; }
+
+}

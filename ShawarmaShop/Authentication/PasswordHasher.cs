7a641c98491e8787417f470240f45f143c11f8d9
Kyryl <kyryl.pochoma@microsoft.wsei.edu.pl>
2025-05-31 15:01:04 +0200

    added class 'User.cs' added class 'PasswordHasher.cs' * AI

---
+++ ShawarmaShop/Authentication/PasswordHasher.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Authentication;
+
+public static class PasswordHasher
+{
+    public static string Hash(string password)
+    {
+        using var sha = SHA256.Create();
+        var bytes = Encoding.UTF8.GetBytes(password);
+        var hash = sha.ComputeHash(bytes);
+        return Convert.ToBase64String(hash);
+    }
+}
